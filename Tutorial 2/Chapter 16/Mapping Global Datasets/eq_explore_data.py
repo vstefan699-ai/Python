8f1e977553e3122ee45ac6204abe3c0ed5b313e6
vstefan699-ai <vstefan699@gmail.com>
2026-04-02 12:32:17 +0200

    Add files via upload

---
+++ Tutorial 2/Chapter 16/Mapping Global Datasets/eq_explore_data.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import json
+
+# Read data as a string and convert to a Python object.
+path = Path(r'C:\Users\vstef\OneDrive\Desktop\CodeCollege\Python-main\Python_Tutorials\Tutorial 2\Chapter 16\Mapping Global Datasets\eq_data\eq_data_1_day_m1.geojson')
+contents = path.read_text(encoding='utf-8')
+all_eq_data = json.loads(contents)
+
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
